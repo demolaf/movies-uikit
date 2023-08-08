69ebaa1012229c81a48431c27134b97a52e5a970
Ademola Fadumo <demolafadumo@gmail.com>
2023-08-08 22:21:44 +0100

    initial commit

---
+++ movies_uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movies_uikit
+//
+//  Created by Ademola Fadumo on 08/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
