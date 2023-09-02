f7778d7a6e976175b51a5c8c918c7f33eaebcca0
Ademola Fadumo <demolafadumo@gmail.com>
2023-09-02 23:44:39 +0100

    feat: movies detail view controller functionality

---
+++ movies_uikit/Modules/Detail/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  movies_uikit
+//
+//  Created by Ademola Fadumo on 02/09/2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
