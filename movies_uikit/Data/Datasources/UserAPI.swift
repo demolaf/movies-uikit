3dd56deae0d731884aa9d2c2003b1a71a6357bba
Ademola Fadumo <demolafadumo@gmail.com>
2023-09-02 23:42:52 +0100

    chore: user api skeleton

--- movies_uikit/Data/Datasources/UserAPI.swift
+++ movies_uikit/Data/Datasources/UserAPI.swift
@@ -10,12 +10,12 @@ import Foundation
 /// For User saved data such as favorite movies and tv shows
 class UserAPI {
     let localStorage: LocalStorage
-    
+
     init(localStorage: LocalStorage) {
         self.localStorage = localStorage
     }
-    
+
     // store for library (movies/tv shows) in realm
-    
+
     // get for library (movies/tv shows) from realm
 }
