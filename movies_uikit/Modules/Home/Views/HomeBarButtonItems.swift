6930f042ad6989e78ce708cee27aa5a4a251bbf6
Ademola Fadumo <demolafadumo@gmail.com>
2023-09-02 23:43:21 +0100

    chore: fix lint errors

--- movies_uikit/Modules/Home/Views/HomeBarButtonItems.swift
+++ movies_uikit/Modules/Home/Views/HomeBarButtonItems.swift
@@ -9,35 +9,35 @@ import Foundation
 import UIKit
 
 class LeadingBarButtonItem: UIBarButtonItem {
-    
+
     let leadingIcon: UIImageView = {
         let drawerIcon = UIImageView(image: UIImage(systemName: "ellipsis"))
         drawerIcon.tintColor = .label
         return drawerIcon
     }()
-    
+
     let navBarTitle: UILabel = {
         //
         let navBarTitle = UILabel()
-        
+
         //
         let firstText = "Play"
         let secondText = "Movies"
-        
+
         //
         let title = "\(firstText) \(secondText)"
         let firstTextRange = title.range(of: firstText)!
         let secondTextRange = title.range(of: secondText)!
-        
+
         //
         let attributedString = NSMutableAttributedString(string: title)
         attributedString.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.label, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24, weight: .regular)], range: NSRange(firstTextRange, in: title))
         attributedString.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.red, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24, weight: .regular)], range: NSRange(secondTextRange, in: title))
         navBarTitle.attributedText = attributedString
-        
+
         return navBarTitle
     }()
-    
+
     let stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
@@ -46,19 +46,19 @@ class LeadingBarButtonItem: UIBarButtonItem {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
-    
+
     override init() {
         super.init()
         setupView()
     }
-    
+
     func setupView() {
         stackView.addArrangedSubview(leadingIcon)
         stackView.addArrangedSubview(navBarTitle)
-        
+
         self.customView = stackView
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError()
     }
