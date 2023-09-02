b1121e9adfce9137030e134fd755b5e6b6dc846b
Ademola Fadumo <demolafadumo@gmail.com>
2023-09-02 23:35:50 +0100

    chore: fix lint errors

--- movies_uikit/Application/AppDelegate.swift
+++ movies_uikit/Application/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    let repositoryProvider = RepositoryProvider()
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -29,6 +31,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
