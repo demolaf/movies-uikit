5493bf9109c537a299ff22e75ac1494d016bb87c
Ademola Fadumo <demolafadumo@gmail.com>
2023-09-02 23:34:51 +0100

    feat: environment variables using xcconfig

--- movies_uikit/Core/Environment.swift
+++ movies_uikit/Core/Environment.swift
@@ -10,21 +10,30 @@ import Foundation
 public enum Environment {
     enum Keys {
         static let tmdbApiKey = "API_KEY"
+        static let tmdbAuthToken = "AUTH_TOKEN"
     }
-    
+
     private static let infoDictionary: [String: Any] = {
         guard let dict = Bundle.main.infoDictionary else {
             fatalError("plist file not found")
         }
-        
+
         return dict
     }()
-    
+
     static let tmdbApiKey: String = {
         guard let apiKeyString = Environment.infoDictionary[Keys.tmdbApiKey] as? String else {
             fatalError("API_KEY not set in plist")
         }
-        
+
         return apiKeyString
+    }()
+
+    static let tmdbAuthToken: String = {
+        guard let authTokenString = Environment.infoDictionary[Keys.tmdbAuthToken] as? String else {
+            fatalError("AUTH_TOKEN not set in plist")
+        }
+
+        return authTokenString
     }()
 }
