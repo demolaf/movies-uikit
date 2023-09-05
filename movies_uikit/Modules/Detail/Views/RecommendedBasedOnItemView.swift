ca6eb782be1a9a42de6d3279842132e7e31747dc
Ademola Fadumo <demolafadumo@gmail.com>
2023-09-05 17:37:34 +0100

    feat: recommendations collection view based on movie in detail view

--- movies_uikit/Modules/Detail/Views/RecommendedBasedOnItemView.swift
+++ movies_uikit/Modules/Detail/Views/RecommendedBasedOnItemView.swift
@@ -7,4 +7,148 @@
 
 import UIKit
 
-class RecommendedBasedOnItemView: UIView {}
+class RecommendedBasedOnItemView: UIView {
+
+    // TODO: Use Generics to fix all the issues with Movie or TVShow type
+    // Replace the below with generics
+    var items: [Movie]?
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
+                return self.createSectionLayout(section: sectionIndex)})
+
+        collectionView.register(
+            SubSectionItemCollectionViewCell.self,
+            forCellWithReuseIdentifier: SubSectionItemCollectionViewCell.reuseId
+        )
+
+        collectionView.register(
+            HeaderCollectionResuableView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: HeaderCollectionResuableView.reuseId
+        )
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        initializeSubviews()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        applyConstraints()
+    }
+
+    private func initializeSubviews() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        self.addSubview(collectionView)
+    }
+
+    private func applyConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 24),
+            collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+
+    func createSectionLayout(
+        section: Int
+    ) -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .absolute(150),
+                heightDimension: .absolute(225)
+            )
+        )
+
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 24)
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .absolute(150),
+                heightDimension: .absolute(275)
+            ),
+            subitems: [item]
+        )
+
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+
+        let footerHeaderSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(50.0)
+        )
+
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: footerHeaderSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+
+        section.boundarySupplementaryItems = [header]
+
+        section.orthogonalScrollingBehavior = .continuous
+        return section
+    }
+
+    func configureViewItems(items: [Movie]) {
+        self.items = items
+        collectionView.reloadData()
+    }
+}
+
+extension RecommendedBasedOnItemView: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 10
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: SubSectionItemCollectionViewCell.reuseId,
+            for: indexPath
+        ) as? SubSectionItemCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let item = items?[indexPath.row]
+        cell.configureViewData(movie: nil)
+        return cell
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        viewForSupplementaryElementOfKind kind: String,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView {
+        guard let headerView = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: HeaderCollectionResuableView.reuseId,
+            for: indexPath
+        ) as? HeaderCollectionResuableView else {
+            return UICollectionReusableView()
+        }
+
+        headerView.configureHeaderLeadingText(leadingText: "Related to this movie")
+        return headerView
+    }
+}
